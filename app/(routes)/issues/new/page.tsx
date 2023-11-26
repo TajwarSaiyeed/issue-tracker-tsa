8de67c207092bce5feee2dbfa5792572d03c04c0
TajwarSaiyeed <rjabid36@gmail.com>
2023-11-27 05:33:19 +0600

    fixed: navigator is not defined

--- app/(routes)/issues/new/page.tsx
+++ app/(routes)/issues/new/page.tsx
@@ -1,6 +1,6 @@
 'use client'
-import {Button, Callout, Text, TextField} from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
+import {Button, Callout, TextField} from "@radix-ui/themes";
+import dynamic from "next/dynamic";
 import {useForm, Controller} from "react-hook-form";
 import axios from 'axios'
 import "easymde/dist/easymde.min.css";
@@ -12,6 +12,9 @@ import {createIssueSchema} from "@/app/validationSchemas";
 import {z} from "zod";
 import ErrorMessage from "@/components/error-message";
 import Spinner from "@/components/spinner";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), { ssr: false });
+
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
