321c3084053cbd47ec6f1c413db0e9c302b54c85
TajwarSaiyeed <rjabid36@gmail.com>
2023-12-03 20:09:31 +0600

    view issue

--- app/(routes)/issues/issue/view/[issueId]/page.tsx
+++ app/(routes)/issues/issue/view/[issueId]/page.tsx
@@ -1,5 +1,8 @@
 import {getIssue} from "@/actions/get-issue";
 import {Metadata} from "next";
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
+import IssueBadge from "@/components/issue-badge";
+import {formattedDate} from "@/lib/utils";
 
 interface IParams {
     params: {
@@ -9,10 +12,26 @@ interface IParams {
 
 const ViewIssue = async ({params: {issueId}}: IParams) => {
     const {issue} = await getIssue(issueId);
+
+
     return (
-        <div>
-            Hello world {issue?.title}
-        </div>
+        <section>
+            <Card>
+                <CardHeader>
+                    <CardTitle>
+                        {issue?.title}
+                    </CardTitle>
+                </CardHeader>
+                <CardContent className={'flex gap-x-2'}>
+                    <IssueBadge status={issue?.status} classname={'rounded-md'}/>
+                    <p className={'text-sm text-gray-400'}>
+                        {(issue?.status === "OPEN" || issue?.status === "IN_PROGRESS") ?
+                            `created ${formattedDate(issue?.createdAt as Date)} days ago`
+                            : `was closed ${formattedDate(issue?.updatedAt as Date)} days ago`}
+                    </p>
+                </CardContent>
+            </Card>
+        </section>
     );
 };
 
