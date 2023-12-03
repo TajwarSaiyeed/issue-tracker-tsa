13df37a097d5bb25cbe5e2ff13d756aec212ac8c
TajwarSaiyeed <rjabid36@gmail.com>
2023-12-03 20:07:57 +0600

    formatted date - created - closed

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formattedDate = (date: Date) => {
+  const d = new Date(date);
+  const now = new Date();
+  const diff = now.getTime() - d.getTime();
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  return `created ${days} days ago`;
+}
