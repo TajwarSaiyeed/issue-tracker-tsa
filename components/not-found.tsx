e57385afd9c3f2eac4b2ef19a75e4bb9274517ff
TajwarSaiyeed <rjabid36@gmail.com>
2023-12-04 07:45:01 +0600

    not-found.tsx added

---
+++ components/not-found.tsx
@@ -0,0 +1,35 @@
+import {FaBug} from "react-icons/fa";
+import {Button} from "@/components/ui/button";
+import Link from "next/link";
+
+const NotFound = ({
+                      title = "Issue not found",
+                      description = "The issue you are looking for does not exist",
+                      href,
+                      btnText = "Back to issues"
+                  }: {
+    title?: string,
+    description?: string
+    href: string
+    btnText: string
+}) => {
+    return (
+        <div className={'flex flex-col justify-center items-center gap-y-5'}>
+            <FaBug className={'text-7xl text-gray-400'}/>
+            <h1
+                className={'text-2xl font-bold text-gray-900 text-center tracking-wide uppercase'}
+            >{title}</h1>
+            <p
+                className={'text-sm text-gray-400'}
+            >{description}</p>
+
+            <Button>
+                <Link href={href}>
+                    {btnText}
+                </Link>
+            </Button>
+        </div>
+    );
+};
+
+export default NotFound;
