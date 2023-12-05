4558a1478bb3bc0c5cf071c6e5ee1b49ec852ffe
TajwarSaiyeed <rjabid36@gmail.com>
2023-12-05 15:50:51 +0600

    chore: show ui based on user session and issue

--- app/(routes)/(dashboard)/_components/latest-issues.tsx
+++ app/(routes)/(dashboard)/_components/latest-issues.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import {Issue} from "@prisma/client";
 import {ScrollArea} from "@/components/ui/scroll-area";
 import IssueBadge from "@/components/issue-badge";
+import EditIssueButton from "@/components/buttons/edit-issue-button";
+import {getSession} from "@/app/api/issues/route";
 
 type LatestIssuesProps = {
     issues: Issue[]
@@ -11,11 +13,12 @@ const LatestIssues = ({issues}: LatestIssuesProps) => {
     return (
         <ScrollArea className="h-96 w-full rounded-md border">
             {issues.map(i => (
-                <div key={i.id} className="p-2 border-b">
-                    <h3 className="text-md font-semibold mb-2">
+                <div key={i.id} className="p-2 border-b px-4">
+                    <h3 className="text-md font-semibold mb-2 flex justify-between items-center">
                         <Link href={`/issues/issue/view/${i.id}`}>
                             {i.title}
                         </Link>
+                        <EditIssueButton userId={i.createdByUserId} issueId={i.id} buttonType={'icon'}/>
                     </h3>
                     <IssueBadge status={i.status} classname={'rounded-md'}/>
                 </div>))}
--- app/(routes)/issues/issue/edit/[issueId]/page.tsx
+++ app/(routes)/issues/issue/edit/[issueId]/page.tsx
@@ -2,6 +2,8 @@ import {Metadata} from "next";
 import {getIssue} from "@/actions/get-issue";
 import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
 import IssueBadge from "@/components/issue-badge";
+import {getSession} from "@/app/api/issues/route";
+import NotFound from "@/components/not-found";
 
 interface IParams {
     params: {
@@ -10,7 +12,37 @@ interface IParams {
 }
 
 const EditIssue = async ({params: {issueId}}: IParams) => {
+    const session = await getSession()
     const {issue} = await getIssue(issueId);
+
+    if (!session) return (
+        <NotFound
+            href={'/'}
+            btnText={"Back To Home"}
+            title={"Need To Sign in"}
+            description={"You need to login to view this page"}
+        />
+    )
+
+    if (!issue) return (
+        <NotFound
+            href={'/'}
+            btnText={"Back To Home"}
+            title={"Issue Not Found"}
+            description={"The issue you are looking for does not exist"}
+        />
+    )
+
+    if (session.user.id !== issue.createdByUserId) return (
+        <NotFound
+            href={'/'}
+            btnText={"Back To Home"}
+            title={"Unauthorized"}
+            description={"You are not authorized to view this page"}
+        />
+    )
+
+
     return (
         <section>
             <Card>
