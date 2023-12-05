03cef13449f296df7c726369508ae6156b93160d
TajwarSaiyeed <rjabid36@gmail.com>
2023-12-05 15:52:00 +0600

    chore: add protection to prevent creating new issue without login

--- components/buttons/create-new-issue.tsx
+++ components/buttons/create-new-issue.tsx
@@ -1,7 +1,16 @@
 import {Button} from "@/components/ui/button";
 import Link from "next/link";
+import {getSession} from "@/app/api/issues/route";
+import {Badge} from "@/components/ui/badge";
 
-const CreateNewIssueButton = () => {
+const CreateNewIssueButton = async () => {
+
+    const session = await getSession()
+
+    if (!session) return <Badge variant={"destructive"} className={'py-2 rounded-md'}>
+        You must be signed in to create a new issue
+    </Badge>
+
     return (
         <Link href={`/issues/new`}>
             <Button variant={"default"} size={"sm"} className={'bg-green-600 hover:bg-green-700'}>
