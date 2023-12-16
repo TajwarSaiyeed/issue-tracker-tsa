0cbb3b1e7f6b6ed3ae5b27995daf19739a6675a3
TajwarSaiyeed <rjabid36@gmail.com>
2023-12-16 19:08:35 +0600

    chore: profile update

--- app/(routes)/(profile)/me/page.tsx
+++ app/(routes)/(profile)/me/page.tsx
@@ -1,20 +1,37 @@
-import {Card, CardHeader, CardTitle} from "@/components/ui/card";
+import {Card, CardContent, CardHeader, CardTitle} from "@/components/ui/card";
 import {Metadata} from "next";
 import {getSession} from "@/lib/utils";
+import Image from "next/image";
 
 const MyProfile = async () => {
     const session = await getSession()
     return (
-        <Card>
-            <CardHeader>
-                <CardTitle>
-                    {session?.user?.name
-                        ? `Welcome ${session?.user?.name}`
-                        : "Welcome"}
-                </CardTitle>
-            </CardHeader>
+        <>
+            <Card>
+                <CardHeader>
+                    <CardTitle>My Profile</CardTitle>
+                </CardHeader>
+                <CardContent className={'flex justify-between items-start'}>
+                    <div>
+                        <h1 className="text-2xl font-semibold">Name</h1>
+                        <p className="text-gray-500">{session?.user?.name}</p>
+                        <h1 className="text-2xl font-semibold">Email</h1>
+                        <p className="text-gray-500">{session?.user?.email}</p>
+                    </div>
+                    <div>
 
-        </Card>
+                        <Image
+                            src={session?.user?.image!}
+                            alt="Picture of the author"
+                            width={100}
+                            height={100}
+                            className="rounded-full"
+                        />
+                    </div>
+                </CardContent>
+
+            </Card>
+        </>
     );
 };
 
